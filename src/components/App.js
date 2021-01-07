bbd007b08afb655db4578ec9c5b2e0a064c914ec
crsandeep24 <crsandeep24@gmail.com>
2021-01-06 19:48:53 -0500

    Added usersReducer & usersHeader

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import PostList from "./PostList";
+import UserHeader from "./UserHeader";
 
 export default function App() {
   return (
     <div className="ui container">
       <PostList />
+      <UserHeader />
     </div>
   );
 }
